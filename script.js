b31a59ad8dd92783db97fac6ca8702c3f73f8f05
Juan Jaimes <jromanjaimes@gmail.com>
2025-09-02 23:31:24 -0500

    Se cambian las URLS de las imagenes del apartado de hobbies

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// Datos de hobbies con imágenes de internet
+// Hobbies con imágenes
 const hobbies = [
-  { nombre: "Ajedrez", imagen: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Chess_board_opening_staunton.jpg" },
-  { nombre: "Videojuegos", imagen: "https://upload.wikimedia.org/wikipedia/commons/2/20/Video_Game_Controller_%28cropped%29.jpg" },
-  { nombre: "Leer", imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Bookshelf.jpg/640px-Bookshelf.jpg" }
+  { nombre: "Ajedrez", imagen: "https://www.clarin.com/2024/08/20/fW3JLI9Fl_2000x1500__1.jpg"},
+  { nombre: "Jugar videojuegos", imagen: "https://media.istockphoto.com/id/1448737609/es/foto/sobre-el-%C3%A1ngulo-del-hombro-de-una-joven-jugadora-que-gana-en-un-videojuego-en-una-computadora.jpg?b=1&s=612x612&w=0&k=20&c=M-9JuAYj8tcLJtQ1wYfErV1erXf5eD-rGc53eHvvY04=" },
+  { nombre: "Leer manga", imagen: "https://pack-yak.intomanga.com/images/manga/Berserk/chapter/1/page/1/15c1d15b-4d03-4c16-967c-2d4b8cebf8eb" },
+  { nombre: "Ver series", imagen: "https://images-cdn4.welcomesoftware.com/assets/top+bingeworthy.jpg/Zz01YzNmZDUzOGEzN2IxMWVmYTcwMmE2OTNhMDk0ZTcyZg==?width=768&height=430" }
 ];
 
-// Datos de series con imágenes públicas
+// Series favoritas con imágenes
 const series = [
+  { nombre: "Better Call Saul", imagen: "https://upload.wikimedia.org/wikipedia/en/1/1b/Better_Call_Saul_logo.svg" },
   { nombre: "Breaking Bad", imagen: "https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png" },
-  { nombre: "Stranger Things", imagen: "https://upload.wikimedia.org/wikipedia/en/f/f7/Stranger_Things_logo.png" },
+  { nombre: "Suits", imagen: "https://upload.wikimedia.org/wikipedia/en/4/42/Suits_season_1_DVD.jpg" },
+  { nombre: "Snowfall", imagen: "https://upload.wikimedia.org/wikipedia/en/5/5c/Snowfall_%28TV_series%29.png" },
+  { nombre: "The Big Bang Theory", imagen: "https://upload.wikimedia.org/wikipedia/en/7/7e/BigBangTheory_Logo.png" },
+  { nombre: "Brooklyn Nine-Nine", imagen: "https://upload.wikimedia.org/wikipedia/en/8/8a/Brooklyn_Nine-Nine_logo.png" },
+  { nombre: "Chernobyl", imagen: "https://upload.wikimedia.org/wikipedia/en/3/3a/Chernobyl_2019_Miniseries.jpg" },
   { nombre: "Dark", imagen: "https://upload.wikimedia.org/wikipedia/en/4/45/DarkNetflixPosterEnglish.jpg" },
-  { nombre: "The Witcher", imagen: "https://upload.wikimedia.org/wikipedia/en/9/9d/The_Witcher_title_card.png" },
-  { nombre: "Game of Thrones", imagen: "https://upload.wikimedia.org/wikipedia/en/d/d8/Game_of_Thrones_title_card.jpg" },
-  { nombre: "Naruto", imagen: "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg" },
-  { nombre: "One Piece", imagen: "https://upload.wikimedia.org/wikipedia/en/2/29/OnePieceVolume1Cover.jpg" },
-  { nombre: "Attack on Titan", imagen: "https://upload.wikimedia.org/wikipedia/en/7/7e/Shingeki_no_Kyojin_manga_volume_1.jpg" },
-  { nombre: "Friends", imagen: "https://upload.wikimedia.org/wikipedia/en/d/d6/Friends_season_one_cast.jpg" },
-  { nombre: "The Mandalorian", imagen: "https://upload.wikimedia.org/wikipedia/en/c/c5/The_Mandalorian_season_2_poster.jpg" }
+  { nombre: "Ragnarok", imagen: "https://upload.wikimedia.org/wikipedia/en/f/f3/Ragnarok_poster.jpg" },
+  { nombre: "Vikings", imagen: "https://upload.wikimedia.org/wikipedia/en/b/b5/Vikings_Title_Card.jpg" }
 ];
 
-// Función para renderizar cards
+// Renderizar galería
 function renderGallery(id, items) {
   const container = document.getElementById(id);
   items.forEach(item => {
@@ -39,6 +40,6 @@ function renderGallery(id, items) {
   });
 }
 
-// Renderizamos todo
+// Pintar en pantalla
 renderGallery("hobbies", hobbies);
 renderGallery("series", series);
