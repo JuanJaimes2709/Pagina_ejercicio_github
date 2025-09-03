fcb4c6662547e81fc1b222190bfbb595301604cd
Juan Jaimes <jromanjaimes@gmail.com>
2025-09-02 23:48:53 -0500

    Se termina de cambiar cada imagen del apartado de series favoritas

--- script.js
+++ script.js
@@ -22,28 +22,28 @@ const series = [
     imagen: "https://static.wikia.nocookie.net/wiki-doblaje-espana/images/a/ad/Breaking_Bad_portada.png/revision/latest?cb=20240325165208&path-prefix=es" },
 
   { nombre: "Suits", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/4/42/Suits_season_1_DVD.jpg" },
+    imagen: "https://www.themoviebuff.net/wp-content/uploads/2024/04/SuitsSO2Gallery04-3d7328f.jpg" },
 
   { nombre: "Snowfall", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/5/5c/Snowfall_%28TV_series%29.png" },
+    imagen: "https://images.justwatch.com/poster/305466252/s718/snowfall.jpg" },
 
   { nombre: "The Big Bang Theory", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/7/7e/BigBangTheory_Logo.png" },
+    imagen: "https://m.media-amazon.com/images/I/71qKFGIrv2L._UF1000,1000_QL80_.jpg" },
 
   { nombre: "Brooklyn Nine-Nine", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/8/8a/Brooklyn_Nine-Nine_logo.png" },
+    imagen: "https://m.media-amazon.com/images/M/MV5BNzBiODQxZTUtNjc0MC00Yzc1LThmYTMtN2YwYTU3NjgxMmI4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
 
   { nombre: "Chernobyl", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/3/3a/Chernobyl_2019_Miniseries.jpg" },
+    imagen: "https://m.media-amazon.com/images/M/MV5BNzU0OTI4YTQtNGQ1ZS00ZjA4LTg3MTMtZjkyZWNjN2RiZDJmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
 
   { nombre: "Dark", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/4/45/DarkNetflixPosterEnglish.jpg" },
+    imagen: "https://m.media-amazon.com/images/M/MV5BOWJjMGViY2UtNTAzNS00ZGFjLWFkNTMtMDBiMDMyZTM1NTY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
 
   { nombre: "Ragnarok", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/f/f3/Ragnarok_poster.jpg" },
+    imagen: "https://resizing.flixster.com/h6RzHQRuGKF_oSKfvYPMyoTzjus=/fit-in/705x460/v2/https://resizing.flixster.com/5j_rdt-u-orKm3_SY9EBuNxzvGU=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vUlRUVjkyODgyMy53ZWJw" },
     
   { nombre: "Vikings", 
-    imagen: "https://upload.wikimedia.org/wikipedia/en/b/b5/Vikings_Title_Card.jpg" }
+    imagen: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9155926_b_v10_au.jpg" }
 ];
 
 // Renderizar galería
